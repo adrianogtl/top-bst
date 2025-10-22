bd3e3ad5cc98395c7b22fbfc6c5e103d9e9a9854
Adriano Garcia <80999356+adrianogtl@users.noreply.github.com>
2025-10-22 11:46:27 -0300

    feat: create `insert`, `deleteItem` and `find` methods of `Tree` class

---
+++ classes.js
@@ -0,0 +1,102 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export default class Tree {
+  constructor(arr) {
+    // Remove duplicates and sort the array
+    const fixedArr = [...new Set(arr)].sort((a, b) => a - b);
+    this.root = this.buildTree(fixedArr);
+  }
+
+  buildTree(arr) {
+    if (arr.length === 0) return null;
+
+    const mid = Math.floor(arr.length / 2);
+    const node = new Node(arr[mid]);
+
+    node.left = this.buildTree(arr.slice(0, mid));
+    node.right = this.buildTree(arr.slice(mid + 1));
+
+    return node;
+  }
+
+  insert(value) {
+    const insertNode = (node, value) => {
+      // Base case
+      if (!node) return new Node(value);
+
+      if (value === node.data) return node;
+
+      // Search for node
+      if (value < node.data) {
+        node.left = insertNode(node.left, value);
+      } else {
+        node.right = insertNode(node.right, value);
+      }
+
+      // Node found
+      return node;
+    };
+
+    insertNode(this.root, value);
+  }
+
+  deleteItem(value) {
+    const deleteNode = (node, value) => {
+      // Base case
+      if (!node) return null;
+
+      // Search for node
+      if (value < node.data) {
+        node.left = deleteNode(node.left, value);
+      } else if (value > node.data) {
+        node.right = deleteNode(node.right, value);
+      } else {
+        // Node found
+        // No children (leaf node)
+        if (!node.left && !node.right) return null;
+
+        // One child
+        if (!node.left) return node.right;
+        if (!node.right) return node.left;
+
+        // Two children
+        // Select the immediately greater node
+        let successor = node.right;
+
+        // Select the smallest child of that node
+        while (successor.left) {
+          successor = successor.left;
+        }
+
+        // Replace with the smallest node's value
+        node.data = successor.data;
+
+        // Delete that smallest node
+        node.right = deleteNode(node.right, successor.data);
+      }
+
+      return node;
+    };
+    deleteNode(this.root, value);
+  }
+
+  find(value) {
+    const findNode = (node, value) => {
+      if (value === node.data || node.data === null) return node;
+
+      if (value < node.data) {
+        return findNode(node.left, value);
+      } else {
+        return findNode(node.right, value);
+      }
+    };
+
+    return findNode(this.root, value);
+  }
+}
---
+++ main.js
@@ -0,0 +1,22 @@
+import Tree from "./classes.js";
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = new Tree(array);
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(tree.root);
+tree.insert(33);
+tree.deleteItem(4);
+prettyPrint(tree.root);
+console.log(tree.find(67));
